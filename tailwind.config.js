770a158697985be0690298fad1e6c941608c1233
hokhoi1979 <166026796+hokhoi1979@users.noreply.github.com>
2025-08-10 09:07:11 +0700

    Build Source

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from "tailwind-scrollbar";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwind-scrollbar")],
+  plugins: [tailwindScrollbar],
 };
